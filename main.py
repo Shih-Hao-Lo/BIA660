138a87dae7aacf2ca611046ed69334ffc474b146
Shih-Hao-Lo <slo@stevens.edu>
2019-12-04 15:29:04 -0500

    add author predictor

--- main.py
+++ main.py
@@ -3,6 +3,77 @@ from nltk.corpus import stopwords
 import networkx as nx
 stopLex=set(stopwords.words('english'))
 import codecs
+from sklearn.metrics import accuracy_score
+
+def loadAuthor(fname,tag,reviews,labels):
+    f=open(fname,encoding='utf8',errors='ignore')
+    for line in f:
+        review=line.strip().lower().split('\t')
+        if len(review)<2: continue
+        if review[0]=='na' or len(review[0])==0:
+            #print(review)
+            continue 
+        reviews.append(review[0].lower())    
+        labels.append(tag)
+    f.close()
+
+def loadAuthor2(fname,reviews,labels):
+    f=open(fname,encoding='utf8',errors='ignore')
+    for line in f:
+        review=line.strip().lower().split('\t')
+        print(review[5])
+        if len(review)<2: continue
+        if review[0]=='na' or len(review[0])==0: continue 
+        reviews.append(review[0].lower())    
+        labels.append(review[5].lower())
+    f.close()
+
+def processtag(fname,reviews,labels):
+    return
+
+def findmax(vote):
+    tmpx=''
+    maxv=0
+    for x in vote:
+        if vote[x]>maxv:
+            tmpx=x
+            maxv=vote[x]
+    return tmpx
+
+def findmax2(vote,vote2):
+    tmpx=''
+    maxv=0
+    maxx=-1
+    for x in vote:
+        if vote[x]>=maxv:
+            if maxx == -1:
+                tmpx=x
+                maxv=vote[x]
+                maxx=x
+            else:
+                if vote[x] == maxv:
+                    if vote2[x] > vote2[maxx]:
+                        tmpx=x
+                        maxv=vote[x]
+                        maxx=x
+                else:
+                    tmpx=x
+                    maxv=vote[x]
+                    maxx=x
+    return tmpx,maxv
+    
+author_test=[]
+author_label=[]
+loadAuthor('cse_test.txt','sigcse',author_test,author_label)
+loadAuthor('siggraph_test.txt','siggraph',author_test,author_label)
+loadAuthor('sigir_test.txt','sigir',author_test,author_label)
+loadAuthor('www_test.txt','www',author_test,author_label)
+loadAuthor('chi_test.txt','sigchi',author_test,author_label)
+loadAuthor('cikm_test.txt','cikm',author_test,author_label)
+loadAuthor('kdd_test.txt','sigkdd',author_test,author_label)
+loadAuthor2('sample.txt',author_test,author_label)
+
+#print(str(author_test))
 
 author = {}
 
@@ -15,9 +86,11 @@ for line in sigirf:
     #print(str(auths))
     for auth in auths:
         if auth.lower() == 'na' or len(auth)==0: continue
-        obj=author.get(auth,{})
+        auth=re.sub('[^a-z]','',auth.lower())
+        obj=author.get(auth,{'tot':0})
         #print('obj:'+str(obj))
-        obj['cse']=obj.get('cse',0)+1
+        obj['sigcse']=obj.get('sigcse',0)+1
+        obj['tot']=obj.get('tot',0)+1
         author[auth]=obj
         #print('obj[sigar]:'+str(obj['sigar']))
     #print(arr)
@@ -30,9 +103,11 @@ for line in wwwf:
     auths=arr[0].split(':')
     for auth in auths:
         if auth.lower() == 'na' or len(auth)==0: continue
-        obj=author.get(auth,{})
+        auth=re.sub('[^a-z]','',auth.lower())
+        obj=author.get(auth,{'tot':0})
         #print('obj:'+str(obj))
         obj['siggraph']=obj.get('siggraph',0)+1
+        obj['tot']=obj.get('tot',0)+1
         author[auth]=obj
         #print('obj[sigar]:'+str(obj['sigar']))
 wwwf.close()
@@ -44,9 +119,11 @@ for line in kddf:
     auths=arr[0].split(':')
     for auth in auths:
         if auth.lower() == 'na' or len(auth)==0: continue
-        obj=author.get(auth,{})
+        auth=re.sub('[^a-z]','',auth.lower())
+        obj=author.get(auth,{'tot':0})
         #print('obj:'+str(obj))
         obj['sigir']=obj.get('sigir',0)+1
+        obj['tot']=obj.get('tot',0)+1
         author[auth]=obj
         #print('obj[sigar]:'+str(obj['sigar']))
 kddf.close()
@@ -59,9 +136,11 @@ for line in sigirf:
     #print(str(auths))
     for auth in auths:
         if auth.lower() == 'na' or len(auth)==0: continue
-        obj=author.get(auth,{})
+        auth=re.sub('[^a-z]','',auth.lower())
+        obj=author.get(auth,{'tot':0})
         #print('obj:'+str(obj))
         obj['www']=obj.get('www',0)+1
+        obj['tot']=obj.get('tot',0)+1
         author[auth]=obj
         #print('obj[sigar]:'+str(obj['sigar']))
     #print(arr)
@@ -75,9 +154,11 @@ for line in sigirf:
     #print(str(auths))
     for auth in auths:
         if auth.lower() == 'na' or len(auth)==0: continue
-        obj=author.get(auth,{})
+        auth=re.sub('[^a-z]','',auth.lower())
+        obj=author.get(auth,{'tot':0})
         #print('obj:'+str(obj))
-        obj['chi']=obj.get('chi',0)+1
+        obj['sigchi']=obj.get('sigchi',0)+1
+        obj['tot']=obj.get('tot',0)+1
         author[auth]=obj
         #print('obj[sigar]:'+str(obj['sigar']))
     #print(arr)
@@ -91,9 +172,11 @@ for line in sigirf:
     #print(str(auths))
     for auth in auths:
         if auth.lower() == 'na' or len(auth)==0: continue
-        obj=author.get(auth,{})
+        auth=re.sub('[^a-z]','',auth.lower())
+        obj=author.get(auth,{'tot':0})
         #print('obj:'+str(obj))
         obj['cikm']=obj.get('cikm',0)+1
+        obj['tot']=obj.get('tot',0)+1
         author[auth]=obj
         #print('obj[sigar]:'+str(obj['sigar']))
     #print(arr)
@@ -107,9 +190,11 @@ for line in sigirf:
     #print(str(auths))
     for auth in auths:
         if auth.lower() == 'na' or len(auth)==0: continue
-        obj=author.get(auth,{})
+        auth=re.sub('[^a-z]','',auth.lower())
+        obj=author.get(auth,{'tot':1})
         #print('obj:'+str(obj))
-        obj['kdd']=obj.get('kdd',0)+1
+        obj['sigkdd']=obj.get('sigkdd',0)+1
+        obj['tot']=obj.get('tot',0)+1
         author[auth]=obj
         #print('obj[sigar]:'+str(obj['sigar']))
     #print(arr)
@@ -118,5 +203,51 @@ sigirf.close()
 #test result
 fw=codecs.open('author_test.txt','w',encoding='utf8')
 for auth in author:
-    if len(author.get(auth,0))>=4: print(auth+':'+str(author.get(auth,0)))
-    fw.write(auth+':'+str(author.get(auth,0))+'\t'+str(len(author.get(auth,0)))+'\n')
+    #if len(author.get(auth,0))>=4: print(auth+':'+str(author.get(auth,0)))
+    fw.write(auth+':'+str(author.get(auth,0))+'\n')
+
+result = []
+result2 = []
+result3 = []
+for x in range(len(author_test)):
+    autharr=author_test[x].split(':')
+    obj={'sigcse':0, 'siggraph':0, 'sigir':0, 'www':0, 'sigchi':0, 'cikm':0, 'sigkdd':0}
+    obj2={'sigcse':0, 'siggraph':0, 'sigir':0, 'www':0, 'sigchi':0, 'cikm':0, 'sigkdd':0}
+    for auth in autharr:
+        auth=re.sub('[^a-z]','',auth.lower())
+        if not auth in author.keys(): continue
+        authobj=author.get(auth)
+        #print(auth,str(authobj))
+        for field in authobj:
+            if field=='tot': continue
+            if obj[field]==0: obj[field]= authobj[field]/authobj['tot']
+            else: obj[field] = obj[field] * (authobj[field]/authobj['tot'])
+            obj2[field] += authobj[field]
+    print(obj)
+    resulttmp = findmax(obj)
+    if resulttmp == '': result.append('na')
+    else: result.append(resulttmp)
+    resulttmp2 = findmax(obj2)
+    if resulttmp2 == '': result2.append('na')
+    else: result2.append(resulttmp2)
+    resulttmp3,tmpv3 = findmax2(obj,obj2)
+    if resulttmp3 == '' or tmpv3==0: result3.append('na')
+    else: result3.append(resulttmp3)
+
+
+
+for x in range(len(result)):
+    if result[x]=='na': author_label[x]='na'
+
+print(author_label)
+
+print(result)
+print(accuracy_score(result,author_label))
+
+print(result2)
+print(accuracy_score(result2,author_label))
+
+print(result3)
+print(accuracy_score(result3,author_label))
+    
+        
--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn import svm
@@ -9,6 +10,8 @@ from sklearn.neural_network import MLPClassifier
 from xgboost import XGBClassifier
 from sklearn.ensemble import VotingClassifier
 import codecs
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import RidgeClassifierCV
 
 def loadData(fname,tag,reviews,labels):
     f=open(fname,encoding='utf8',errors='ignore')
@@ -20,6 +23,17 @@ def loadData(fname,tag,reviews,labels):
             continue 
         reviews.append(review[1].lower())    
         labels.append(tag)
+    f.close()
+    
+def loadAbs2(fname,reviews,labels):
+    f=open(fname,encoding='utf8',errors='ignore')
+    for line in f:
+        review=line.strip().lower().split('\t')
+        #print(review)
+        if len(review)<2: continue
+        if review[1]=='na' or len(review[1])==0: continue 
+        reviews.append(review[1].lower())    
+        labels.append(review[5].lower())
     f.close()
 
 def findmax(vote):
@@ -33,25 +47,25 @@ def findmax(vote):
 
 reviews=[]
 labels=[]
-loadData('cse_train.txt','cse',reviews,labels)
+loadData('cse_train.txt','sigcse',reviews,labels)
 loadData('siggraph_train.txt','siggraph',reviews,labels)
 loadData('sigir_train.txt','sigir',reviews,labels)
 loadData('www_train.txt','www',reviews,labels)
-loadData('chi_train.txt','chi',reviews,labels)
+loadData('chi_train.txt','sigchi',reviews,labels)
 loadData('cikm_train.txt','cikm',reviews,labels)
-loadData('kdd_train.txt','kdd',reviews,labels)
+loadData('kdd_train.txt','sigkdd',reviews,labels)
 
 
 rev_test=[]
 labels_test=[]
-loadData('cse_test.txt','cse',rev_test,labels_test)
+loadData('cse_test.txt','sigcse',rev_test,labels_test)
 loadData('siggraph_test.txt','siggraph',rev_test,labels_test)
 loadData('sigir_test.txt','sigir',rev_test,labels_test)
 loadData('www_test.txt','www',rev_test,labels_test)
-loadData('chi_test.txt','chi',rev_test,labels_test)
+loadData('chi_test.txt','sigchi',rev_test,labels_test)
 loadData('cikm_test.txt','cikm',rev_test,labels_test)
-loadData('kdd_test.txt','kdd',rev_test,labels_test)
-
+loadData('kdd_test.txt','sigkdd',rev_test,labels_test)
+loadAbs2('sample.txt',rev_test,labels_test)
 
 #print(labels_test)
 counter = CountVectorizer(stop_words='english')
@@ -61,21 +75,26 @@ counter.fit(reviews)
 counts_train = counter.transform(reviews)#transform the training data
 counts_test = counter.transform(rev_test)#transform the testing data
 
-#print(counts_train)
+print(counts_train.shape)
 #print(labels)
 #print(counts_test)
 
+tfidf_transformer  = TfidfTransformer(use_idf=False).fit(counts_train)
+X_train_tfidf = tfidf_transformer.fit_transform(counts_train)
+X_test_tfidf = tfidf_transformer.fit_transform(counts_test)
+
 clf1 = MultinomialNB()
 clf2 = svm.SVC(gamma='scale', decision_function_shape='ovo')
-clf3 = svm.LinearSVC()
+clf3 = svm.LinearSVC(multi_class='ovr', max_iter=3000)
 clf4 = MLPClassifier()
-clf5 = SGDClassifier(loss="hinge", penalty="l2", max_iter=15)
+clf5 = SGDClassifier(n_jobs=7, loss="hinge", penalty="l2", max_iter=3000)
 #clf6 = KNeighborsClassifier(n_neighbors=3)
 clf7 = XGBClassifier(max_depth=10,colsample_bytree=0.9)
 #clf7 = XGBClassifier(learning_rate =0.01,n_estimators=5000,max_depth=4,min_child_weight=6,gamma=0,subsample=0.8,colsample_bytree=0.8,reg_alpha=0.005,objective= 'binary:logistic',nthread=4,scale_pos_weight=1,seed=27) 
-clf = [clf1, clf2, clf3, clf4, clf5, clf7]
-arr = ['MultinomialNB','SVC','LinearSVC','MLPClassifier','SGDClassifier','XGBClassifier']
-predictors = [('nb',clf1), ('svc',clf2), ('lsvc',clf3), ('mlp',clf4), ('sgd',clf5), ('xgbc',clf7)]
+clf8 = RidgeClassifierCV()
+clf = [clf1, clf2, clf3, clf4, clf5, clf7, clf8]
+arr = ['MultinomialNB','SVC','LinearSVC','MLPClassifier','SGDClassifier','XGBClassifier','RidgeClassifierCV']
+#predictors = [('nb',clf1), ('svc',clf2), ('lsvc',clf3), ('mlp',clf4), ('sgd',clf5), ('xgbc',clf7)]
 
 fw=codecs.open('vote_test.txt','w',encoding='utf8')
 
@@ -94,23 +113,22 @@ for x in range(len(clf)):
     #print('correct answer:',labels_test)
     s=accuracy_score(pred,labels_test)
     #print (s)
-    print(str(len(pred)))
     result.append(s)
     if s > maxp:
         maxp = s
         maxx = x
         maxpred = pred
+
 fw.write(str(predarr))
 fw.close()
+
 for x in range(len(result)):
     print(arr[x]+':'+str(result[x]))
-
-
 
 finalarr=[]
 
 for x in range(len(rev_test)):
-    vote = {'cse':0, 'siggraph':0, 'sigir':0, 'www':0, 'chi':0, 'cikm':0, 'kdd':0}
+    vote = {'sigcse':0, 'siggraph':0, 'sigir':0, 'www':0, 'sigchi':0, 'cikm':0, 'sigkdd':0}
     for y in range(len(predarr)):
         vote[predarr[y][x]]+=1
     #print(vote)
